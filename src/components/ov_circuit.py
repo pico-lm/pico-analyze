ca901b6481f1ebfbe8c7326f2a861b4851398664
Richard Diehl Martinez <richarddiehl@gmail.com>
2025-03-19 07:16:23 +0000

    returning full concated circuit in ov_circuit in addition to heads

--- src/components/ov_circuit.py
+++ src/components/ov_circuit.py
@@ -4,7 +4,7 @@ OV circuit is a compound component that is composed of the OV circuit layers.
 """
 
 from functools import lru_cache
-from typing import Any, Dict
+from typing import Any, Dict, Tuple
 
 # typing imports
 import torch
@@ -22,6 +22,8 @@ class OVComponent(BaseComponent):
     that in an attention module, the value and the output projections always operate jointly,
     and write into the 'residual stream'. Thus, it makes sense to treat the Output and Value
     matrices as a single 'OV-Circuit' matrix, which we can compute the activations and weights for.
+
+    Also note that the OV-Circuit operates 'per head' of the attention module.
 
     To read more about the OV-Circuit, see:
         https://transformer-circuits.pub/2021/framework/index.html
@@ -41,19 +43,24 @@ class OVComponent(BaseComponent):
         self,
         layer_value_activation: torch.Tensor,
         layer_output_projection: torch.Tensor,
-    ) -> torch.Tensor:
+    ) -> Tuple[Dict[str, torch.Tensor], torch.Tensor]:
         """
         Compute the OV activations for a single layer. Uses a cache to speed up the computation,
         if the component is used across multiple metrics.
+
+        NOTE: the OV-Circuit operates 'per head' of the attention module, so we compute the OV
+        activations for each head separately and then concatenate them together.
 
         Args:
             layer_value_activation: The value activations for the layer.
             layer_output_projection: The output projection for the layer.
 
         Returns:
-            A dictionary mapping head indices to OV component activations.
+            A tuple:
+                - A dictionary mapping head indices to OV component activations.
+                - A concatenated tensor of the OV component activations.
         """
-        layer_ov_activation = {}
+        layer_ov_activation_per_head = {}
 
         for head_idx in range(self.attention_n_heads):
             kv_head_idx = head_idx // (
@@ -79,22 +86,38 @@ class OVComponent(BaseComponent):
                 ].T
             )
 
-            layer_ov_activation[f"{head_idx}"] = ov_activation_per_head
+            layer_ov_activation_per_head[f"{head_idx}"] = ov_activation_per_head
 
-        return layer_ov_activation
+        layer_ov_activation = torch.cat(
+            list(layer_ov_activation_per_head.values()), dim=1
+        )
+
+        return layer_ov_activation_per_head, layer_ov_activation
 
     @lru_cache(maxsize=50)
     def compute_ov_weights(
         self,
         layer_value_projection: torch.Tensor,
         layer_output_projection: torch.Tensor,
-    ) -> torch.Tensor:
+    ) -> Tuple[Dict[str, torch.Tensor], torch.Tensor]:
         """
         Compute the OV weights for a single layer. Uses a cache to speed up the computation,
         if the component is used across multiple metrics.
+
+        NOTE: the OV-Circuit operates 'per head' of the attention module, so we compute the OV
+        weights for each head separately and then concatenate them together.
+
+        Args:
+            layer_value_projection: The value projection for the layer.
+            layer_output_projection: The output projection for the layer.
+
+        Returns:
+            A tuple:
+                - A dictionary mapping head indices to OV component weights.
+                - A concatenated tensor of the OV component weights.
         """
 
-        layer_ov_weights = {}
+        layer_ov_weights_per_head = {}
 
         for head_idx in range(self.attention_n_heads):
             kv_head_idx = head_idx // (
@@ -107,14 +130,18 @@ class OVComponent(BaseComponent):
             start_output_projection = head_idx * self.attention_head_dim
             end_output_projection = (head_idx + 1) * self.attention_head_dim
 
-            layer_ov_weights[f"{head_idx}"] = (
+            ov_weights_per_head = (
                 layer_value_projection[start_value_projection:end_value_projection, :]
                 @ layer_output_projection[
                     :, start_output_projection:end_output_projection
                 ]
             )
 
-        return layer_ov_weights
+            layer_ov_weights_per_head[f"{head_idx}"] = ov_weights_per_head
+
+        layer_ov_weights = torch.cat(list(layer_ov_weights_per_head.values()), dim=1)
+
+        return layer_ov_weights_per_head, layer_ov_weights
 
     @lru_cache(maxsize=50)
     def compute_ov_gradients(
@@ -123,17 +150,31 @@ class OVComponent(BaseComponent):
         layer_output_gradient: torch.Tensor,
         layer_value_projection: torch.Tensor,
         layer_output_projection: torch.Tensor,
-    ) -> torch.Tensor:
+    ) -> Tuple[Dict[str, torch.Tensor], torch.Tensor]:
         """
         Compute the OV gradients for a single layer. Uses a cache to speed up the computation,
         if the component is used across multiple metrics.
 
+        NOTE: the OV-Circuit operates 'per head' of the attention module, so we compute the OV
+        gradients for each head separately and then concatenate them together.
+
         The gradients are computed using the chain rule and is composed of two parts:
             1. dL/dOutput * dOutput/dOV
             2. dL/dValue * dValue/dOV
+
+        Args:
+            layer_value_gradient: The gradient of the value projection for the layer.
+            layer_output_gradient: The gradient of the output projection for the layer.
+            layer_value_projection: The value projection for the layer.
+            layer_output_projection: The output projection for the layer.
+
+        Returns:
+            A tuple:
+                - A dictionary mapping head indices to OV component gradients.
+                - A concatenated tensor of the OV component gradients.
         """
 
-        layer_ov_gradients = {}
+        layer_ov_gradients_per_head = {}
 
         for head_idx in range(self.attention_n_heads):
             kv_head_idx = head_idx // (
@@ -167,9 +208,13 @@ class OVComponent(BaseComponent):
                 head_output_gradient @ head_value_projection
             )
 
-            layer_ov_gradients[f"{head_idx}"] = head_ov_gradient
+            layer_ov_gradients_per_head[f"{head_idx}"] = head_ov_gradient
 
-        return layer_ov_gradients
+        layer_ov_gradients = torch.cat(
+            list(layer_ov_gradients_per_head.values()), dim=1
+        )
+
+        return layer_ov_gradients_per_head, layer_ov_gradients
 
     def validate_component(self, component_config: BaseComponentConfig) -> None:
         """
@@ -193,6 +238,10 @@ class OVComponent(BaseComponent):
         Generates the OV circuit component. The OV circuit component is a compound component
         that is composed of the value and output projections of the model.
 
+        NOTE: the OV-Circuit operates 'per head' of the attention module, so we compute and return
+          the OV activations, weights, and gradients for each head separately and then concatenate
+          them together as well into one aggregated tensor.
+
         Args:
             checkpoint_states: Dict[str, Dict[str, torch.Tensor]] -- the checkpoint states
                 to generate the component for.
@@ -214,6 +263,9 @@ class OVComponent(BaseComponent):
                 f"{_model_prefix}{layer_idx}.{layer_suffixes['output_layer']}"
             ]
 
+            # NOTE: depending on whether we are computing activations, weights, or gradients, we
+            # need to fetch the appropriate tensors from the checkpoint states
+
             if component_config.data_type == "activations":
                 layer_value_activation = checkpoint_states["activations"][
                     f"{_model_prefix}{layer_idx}.{layer_suffixes['value_layer']}"
@@ -231,25 +283,33 @@ class OVComponent(BaseComponent):
                     f"{_model_prefix}{layer_idx}.{layer_suffixes['value_layer']}"
                 ]
 
+            # NOTE: each computation is done 'per head' of the attention module, and return a
+            # tuple of a dictionary mapping head indices to the OV component and a concatenated
+            # tensor of the OV component
+
             if component_config.data_type == "activations":
-                ov_component = self.compute_ov_activations(
+                ov_component_per_head, ov_component = self.compute_ov_activations(
                     layer_value_activation, layer_output_projection
                 )
             elif component_config.data_type == "weights":
-                ov_component = self.compute_ov_weights(
+                ov_component_per_head, ov_component = self.compute_ov_weights(
                     layer_value_projection, layer_output_projection
                 )
             elif component_config.data_type == "gradients":
-                ov_component = self.compute_ov_gradients(
+                ov_component_per_head, ov_component = self.compute_ov_gradients(
                     layer_value_gradient,
                     layer_output_gradient,
                     layer_value_projection,
                     layer_output_projection,
                 )
 
-            for head_idx, ov_component_head in ov_component.items():
+            for head_idx, ov_component_head in ov_component_per_head.items():
                 checkpoint_layer_component[
                     f"{_model_prefix}{layer_idx}.ov_circuit.{component_config.data_type}.heads.{head_idx}"
                 ] = ov_component_head
+
+            checkpoint_layer_component[
+                f"{_model_prefix}{layer_idx}.ov_circuit.{component_config.data_type}"
+            ] = ov_component
 
         return checkpoint_layer_component
